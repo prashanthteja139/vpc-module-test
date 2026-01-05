55eec79b736a3a01fbcc66dfaf8fdff4b49d014d
Prashanth Teja <100507992+prashanthteja139@users.noreply.github.com>
2026-01-05 23:17:02 +0530

    Clarify is_peering_required variable with comment
    Added comment to clarify the purpose of the 'is_peering_required' variable.

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "vpc" {
     public_subnet_cidrs = var.public_subnet_cidrs
     private_subnet_cidrs = var.private_subnet_cidrs
     database_subnet_cidrs = var.database_subnet_cidrs
-
+#if peering required keep true
     is_peering_required = true
 }
 
