61fd2cc4f6e1dec39036977782473d408018af7a
Prashanth Teja <100507992+prashanthteja139@users.noreply.github.com>
2026-01-16 22:55:29 +0530

    Add comment for public_subnet_cidrs variable
    Added comment to clarify purpose of public_subnet_cidrs variable

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
-
+#assign cidr range according to requirements 
 variable "public_subnet_cidrs" {
     default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
